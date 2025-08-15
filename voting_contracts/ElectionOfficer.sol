bd915963602803ba2cf179aa03caa903693d81b4
T-SarvesH <sarvesh1.tikekar@gmail.com>
2025-08-15 19:05:52 +0530

    Almost working voters and Elec officers contracts

--- voting_contracts/ElectionOfficer.sol
+++ voting_contracts/ElectionOfficer.sol
@@ -3,9 +3,12 @@ pragma solidity ^0.8.20;
 
 contract ElectionOfficer{
 
-    //Timestart and Timeend for a limited registration window
+    //Timestart and Timeend for a limited registration window (Will not change even if multiple contract instances are deployed)
     uint immutable timeStart = block.timestamp;
     uint immutable timeEnd = timeStart + 3 days;
+
+    uint immutable electionStart = timeEnd + 1 weeks;
+    uint immutable electionEnd = electionStart + 1 days;
 
     struct electionOfficer{
 
@@ -14,62 +17,65 @@ contract ElectionOfficer{
         uint allotedConstituency;
     }
 
-    struct Map{
-
-        address mapper;
-        electionOfficer e;
-    }
-
-    
     //All important members 
+    mapping(address => electionOfficer) Map;
+    mapping (address => bool) isOfficer;
+    mapping (uint => bool) constituencyMapping;
 
-    //Set to 1 as electionCommissioner has to be there
+    address [] electionOfficers;
+    
     uint electionOfficerCount = 0;
-    bool isElecCommssionerSet = false;
-
     address public immutable electionCommissioner;
-    Map [] electionOfficers;
 
-    modifier isElecComsnrSet(){
+    event OfficerAssigned(address officer, uint constituency);
 
-        require(isElecCommssionerSet==false, "The Election Commissioner is already elected");
+    modifier onlyElectionCommissioner(){
+
+        require(msg.sender == electionCommissioner, "Only the Election Commissioner can perform this action");
         _;
     }
 
-    modifier isElecComsnr(){
+    modifier registrationOpen(){
 
-        require(msg.sender == electionCommissioner, "Only the Election Commissioner can elect the new officers");
+        require(block.timestamp <= timeEnd , "The Time for registering new officers has elapsed");
         _;
     }
 
-    modifier isTimeElapsed(){
 
-        require(block.timestamp <= timeEnd && block.timestamp >= timeStart, "The Time for registering new officers has elapsed");
-        _;
+    function isElecOfficer(address officerAddress) public view returns (bool){
+
+        return isOfficer[officerAddress];
     }
     
-    function electElectionOfficers(string calldata name, uint id, uint allotedConstituency, address Officer) public isElecComsnr isTimeElapsed returns (string memory){
+    //Elect election officers (Permission: Election Commissioner)
+    function electElectionOfficers(address officerAddress, string calldata name, uint id, uint allotedConstituency) public onlyElectionCommissioner registrationOpen{
+
+        require(id > 0, "Officer ID must be positive");
+        require(allotedConstituency > 0, "Constituency must be positive");
+        require(!constituencyMapping[allotedConstituency], "This constituency already has a officer alloted");
+        require(!isOfficer[officerAddress], "This officer is already elected");
 
         electionOfficer memory eo = electionOfficer(name, id, allotedConstituency);
-        Map memory M = Map(Officer, eo);
-        electionOfficers.push(M);
+        Map[officerAddress] = eo;
+        isOfficer[officerAddress] = true;
+        constituencyMapping[allotedConstituency] = true;
+        electionOfficers.push(officerAddress);
         ++electionOfficerCount;
-        return "The election officer is now successfully assigned";
+
+        emit OfficerAssigned(officerAddress, allotedConstituency);
     }
 
+    //Get officer by Address
     function getOfficerByAddress(address officerAddress) public view returns (electionOfficer memory){
 
-        for(uint i=0; i<electionOfficers.length; ++i){
+        bool dec = isElecOfficer(officerAddress);
+        require(dec, "The address is not of a Election Officer");
 
-            if(electionOfficers[i].mapper == officerAddress)
-            return electionOfficers[i].e;
-        }
-        
+        return Map[officerAddress];
     }
 
     constructor () {
 
-        isElecCommssionerSet = true;
         electionCommissioner = address(msg.sender);
     }
 }
--- voting_contracts/Voter.sol
+++ voting_contracts/Voter.sol
@@ -10,23 +10,19 @@ contract Voter{
     uint immutable startTime = block.timestamp;
     uint immutable endTime = startTime + 1 weeks;
 
+    uint immutable electionStart = endTime + 1 weeks;
+    uint immutable electionEnd = electionStart + 1 days;
+
     ElectionOfficer e;
-    ElectionOfficer.Map []  public electionOfficers;
-
-    constructor(address _ElectionOfficerAddr){
-
-        e = ElectionOfficer(_ElectionOfficerAddr);
-        electionOfficers = e.getAllOfficers();
-    }
-
     
     //Get a mapping of Officers and constituencies assigned
     struct voter{
 
+        uint id;
         string name;
         uint age;
         bytes32 aadharNumber;
-        bytes32 voterId;
+        bytes32 voterIdNumber;
         uint ConstituencyId;
         bool hasVoted;
         bool hasRegistered;
@@ -36,26 +32,48 @@ contract Voter{
 
     mapping(address => voter) voterMap;
     address [] voterAddresses;
-
     int public voterCount = 0;
 
-    modifier isEndTimeLapsed(){
+    // From ElectionOfficer Module
+    modifier registrationOpen(){
 
         require(block.timestamp <= endTime && block.timestamp >= startTime, "The registration period is over");
         _;
     } 
 
+    modifier isOfficeFromSameConstituency(address officerAddress, address voterAddress){
+        
+        require(
+            e.isElecOfficer(officerAddress) 
+            && e.getOfficerByAddress(officerAddress).allotedConstituency == voterMap[voterAddress].ConstituencyId, 
+            "Cannot verify the voter as both are from different constituencies"
+        );
 
-    function registerAsVoter(string calldata name,
-        uint age,
-        bytes12 aadharNumber,
-        string memory voterId,
-        uint ConstituencyId) public returns  (string memory){
+        _;
+    }
 
-            bytes32 hashedAadhar = keccak256(abi.encodePacked(aadharNumber));
-            bytes32 hashedVoterId = keccak256(abi.encodePacked(voterId));
+    modifier isElectionLive (){
+
+        require(block.timestamp <= electionEnd && block.timestamp >= electionStart, "The election has ended");
+        _;
+    }
+
+    modifier isVoterVerified(address voterAddress){
+
+        require(!voterMap[voterAddress].isAllowedToVote, "Voter is already verified");
+        _;
+    }   
+
+    function registerAsVoter(uint _id, string calldata _name,
+        uint _age,
+        bytes12 _aadharNumber,
+        string memory _voterIdNumber,
+        uint _ConstituencyId) public registrationOpen returns (string memory){
+
+            bytes32 hashedAadhar = keccak256(abi.encodePacked(_aadharNumber));
+            bytes32 hashedVoterId = keccak256(abi.encodePacked(_voterIdNumber));
             
-            voter memory v = voter(name, age, hashedAadhar, hashedVoterId, ConstituencyId, false, false, false, -1);
+            voter memory v = voter(_id, _name, _age, hashedAadhar, hashedVoterId, _ConstituencyId, false, true, false, -1);
             voterMap[address(msg.sender)] = v;
             voterAddresses.push(address(msg.sender));
             ++voterCount;
@@ -63,12 +81,28 @@ contract Voter{
             return "The voter is registered successfully. Now he awaits for approval";
     }
 
-    function verifyVoters(bytes12 aadharNumber,
-        string memory voterId, bool decision) public view{
+    function verifyVoters(address voterAddress, bytes12 _aadharNumber,
+        string memory _voterIdNumber, bool decision) isOfficeFromSameConstituency(address(msg.sender), voterAddress) isVoterVerified(voterAddress) public returns (string memory){
+            
+            //For verification we using aadhar card and VoterId
+            bytes32 hashedAadhar = keccak256(abi.encodePacked(_aadharNumber));
+            bytes32 hashedVoterId = keccak256(abi.encodePacked(_voterIdNumber));
 
-            bytes32 hashedAadhar = keccak256(abi.encodePacked(aadharNumber));
-            bytes32 hashedVoterId = keccak256(abi.encodePacked(voterId));
+            voter storage v = voterMap[voterAddress];
+            require(v.id > 0, "Voter not present in DB");
+            require(v.aadharNumber == hashedAadhar, "Aadhar card number mismatch");
+            require(v.voterIdNumber == hashedVoterId, "Voter ID number mismatch");
 
+            if(decision){
+                v.isAllowedToVote = decision;
+            }
+        
+            return decision? "Voter Successfully verified and can vote": "Voter not registered as not following the specified rules";
+    }
+
+    constructor (address _ElectionOfficerAddr) {
+
+        e = ElectionOfficer(_ElectionOfficerAddr);
     }
     
 }
